5b12ca7688fb32bf8123f4578bca2befb65f728e
Jeena <jmj.jeena@gmail.com>
2018-11-06 21:13:35 -0800

    Added Test component

---
+++ src/Component/Test.js
@@ -0,0 +1,11 @@
+// import React, { Component } from 'react';
+// import VenueList from './venue-list';
+ // export default class TestBar extends Component {
+// 	render() {
+// 		return (
+// 			<div className="testbar">
+// 				<VenueList />
+// 			</div>
+// 		);
+// 	}
+// } 
