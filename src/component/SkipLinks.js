7d6d87f607031517f5968510c4332c919abb9c99
Jeena <jmj.jeena@gmail.com>
2018-11-09 13:28:03 -0800

    refactored into a functional-stateless

--- src/component/SkipLinks.js
+++ src/component/SkipLinks.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class SkipLinks extends Component {
-    render() {
-        return (
-            <ul className="skip-links">
-                <li>
-                    <a href="#search">Skip To Search Field</a>
-                </li>
-                <li>
-                    <a href="#info-cards">Skip To Info Cards</a>
-                </li>
-            </ul>
-        );
-    }
-}
+const SkipLinks = () => {
+    return (
+        <ul className="skip-links">
+            <li>
+                <a href="#search">Skip To Search</a>
+            </li>
+            <li>
+                <a href="#info-cards">Skip To Info Cards</a>
+            </li>
+        </ul>
+    );
+}  
+
+export default SkipLinks;
